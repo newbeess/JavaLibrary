5684e26c653f78066629327ed22b6d0ccadc86a8
newbeess <newbeessfu@gmail.com>
2016-04-15 13:32:28 +0800

    DaemonThreadPoolExecutor

---
+++ com/elephant/util/DaemonThreadPoolExecutor.java
@@ -0,0 +1,9 @@
+package com.elephant.util;
+
+import java.util.concurrent.*;
+
+public class DaemonThreadPoolExecutor extends ThreadPoolExecutor{
+	public DaemonThreadPoolExecutor(){
+		super(0,Integer.MAX_VALUE,60L,TimeUnit.SECONDS,new SynchronousQueue<Runnable>(),new DaemonThreadFactory());
+	}
+}
