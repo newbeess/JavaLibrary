f431126726f461ea2c329a7a5292bb3fd252ab41
newbeess <newbeessfu@gmail.com>
2016-04-26 20:43:29 +0800

    add BinarySearch

---
+++ data_structure/src/main/java/com/elephant/data_structure/BinarySearch.java
@@ -0,0 +1,7 @@
+package com.elephant.data_structure;
+
+/**
+ * Created by elephant on 16/4/24.
+ */
+public class BinarySearch {
+}
---
+++ data_structure/src/main/java/com/elephant/data_structure/queue/Queue.java
@@ -0,0 +1,7 @@
+package com.elephant.data_structure.queue;
+
+/**
+ * Created by elephant on 16/4/23.
+ */
+public class Queue {
+}
